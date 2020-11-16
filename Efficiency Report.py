56b0699bc8f9e6221d000f847216031db7e69160
austinshumway <66091799+austinshumway@users.noreply.github.com>
2020-11-16 09:02:39 -0700

    Update Efficiency Report.py

--- Efficiency Report.py
+++ Efficiency Report.py
@@ -7,17 +7,20 @@ import numpy as np
 
 #### Reads in the Excel files for the Proposal Form and Resource Allocation.
 ### We need to transition these onto the company portal so we don't have to keep switching the directories depending on who runs the program
-proposal_form = pd.read_excel('C:\Users\Austin\OneDrive - The Cicero Group\Cicero\Proposal Form.xlsx')
+proposal_form = pd.read_excel(r'C:\Users\Austin\OneDrive - The Cicero Group\Cicero\Proposal Form.xlsx')
 resource_allocation = r'C:\Users\Austin\The Cicero Group\Company Portal - Resource Allocation\2020 Resource Allocation (Cicero).xlsx'
 billable_projects = pd.read_excel(resource_allocation, 'Billable Projects', skiprows=1)
 billable_projects_completed = pd.read_excel(resource_allocation, 'Billable Projects (Completed)', skiprows=1)
 ed_direction = pd.read_excel(resource_allocation, 'Ed Direction', skiprows=1)
 ra = pd.concat([billable_projects, billable_projects_completed, ed_direction])
 
+## I noticed a function like this in Isaac's code so I just quickly wrote it out in case it needed to be used later
+##roles_renamed = ra.rename(columns={'Partner Hours':'PART', 'Principal Hours':'PRIN', 'Engagement Manager Hours':'EM', 'Associate Hours':'ASSOC', 'Analyst Hours':'BA'}, inplace=True)
 
 ###insert melt function here
+##currently not working. I don't know why it won't select data in a range
+ra_combined = ra.melt(id_vars='date', col_level=['1/1/2020']:['4/26/2021'])
 
-##does this show up on Isaac's Program
 
 
 today = date.today()
